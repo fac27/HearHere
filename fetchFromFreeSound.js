c1e33426435ecbe907e0168320155f47ae7ccc2b
simonryrie <simonryrie@gmail.com>
2023-02-11 12:46:57 +0000

    function to get sound url for given country written
    Relates #6

--- fetchFromFreeSound.js
+++ fetchFromFreeSound.js
@@ -3,12 +3,13 @@ let countryTwo = ""
 
 async function getCountrySounds(countryOne) {
     try {
-      const response = await fetch(`https://example.com/sounds?tag=${countryOne}`);
+      const response = await fetch(`https://freesound.org/apiv2/search/text/?query=${countryOne}&token=${key}`);
       const sounds = await response.json();
-      const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
-      const soundResponse = await fetch(randomSound.url);
+      const randomSound = sounds.results[Math.floor(Math.random() * sounds.count)];
+      const soundId = randomSound.id;
+      const soundResponse = await fetch(`https://freesound.org/apiv2/sounds/${soundId}?token=${key}`);
       const soundData = await soundResponse.json();
-      const soundPreviewUrl = soundData.preview_url;
+      const soundPreviewUrl = soundData.previews['preview-hq-mp3'];
       return soundPreviewUrl;
     } catch (error) {
       console.error(error);
---
+++ secret.js
@@ -0,0 +1,1 @@
+export const key = "2vHtoiDeRkF6wnNuWU8bxQLLM6KtCLV8MffwomA2";
